afb258167586c049c9a40b47444b938b07aa7e41
Ricarda <42298717+Hoch3007@users.noreply.github.com>
2019-02-06 17:06:51 +0100

    Create basiccalculator.py

---
+++ 07/basiccalculator.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    
+    import operator
+    ops = { "+": operator.add, "-": operator.sub, "*":operator.mul, "/": operator.div }
+    
+    calculation = raw_input("What do you want to calculate? Format: number operation number (incl. white space)" )
+    
+    calculation = calculation.split(" ")
+    number1 = int(calculation[0])
+    operation = calculation[1]
+    number2 = int(calculation[2])
+    
+    print ops[operation](number1,number2)
+    
