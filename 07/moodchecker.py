852632f514df9aed0760b37966f0714843a636eb
Ricarda <wunderpott@gmail.com>
2019-02-08 19:02:47 +0100

    changed input string

--- 07/moodchecker.py
+++ 07/moodchecker.py
@@ -1,10 +1,10 @@
 if __name__ == "__main__":
     
-    mood = input("How's your mood today? Choose from: sad, happy, nervous, excited, relaxed. Your choice: " )
-    
     moods = {"sad": "Don't be! Life is beautiful.", "happy": "It is great to see you happy!",
              "nervous": "Take a deep breath 3 times.",
              "excited": "Great. I hope it's positive excitement.", "relaxed": "Cool."}
+
+    mood = input("How's your mood today? Choose from: " + str(moods.keys()) + " Your choice: ")
 
     if mood in moods:
         print(moods[mood])
