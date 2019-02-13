b0ab4b2921ec0491b7548839024935fd7ee751b1
Ricarda <42298717+Hoch3007@users.noreply.github.com>
2019-02-13 09:34:07 +0100

    Create fizzbuzz.py
    still python 2.7

---
+++ 08/fizzbuzz.py
@@ -0,0 +1,30 @@
+if __name__ == "__main__":
+    
+    print "----- FIZZ BUZZ -----"
+    
+    again = True
+    
+    while again:
+    
+        number = raw_input("Select a number between 1 and 100! " )
+        
+        for i in range(1,int(number)):
+        
+            if i%15==0:
+                print "fizzbuzz"
+
+            elif i%5==0:
+                print "buzz"
+
+            elif i%3==0:
+                print "fizz"
+            else:
+                print i
+                
+        answer = raw_input("Would you like to play again! y/n" )
+        
+        answer = answer.lower()
+    
+        if answer == "n":
+            again = False
+            print "Thanks for playing FizzBuzz."
